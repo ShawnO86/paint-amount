bf72ecc93c6785c9b857d81e817547e1a98275b8
Shawn <shawnby222@gmail.com>
2022-11-10 12:43:36 -0600

    update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:ShawnO86/ShawnO86.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ShawnO86/paint-amount.git main:gh-pages
+git push -f https://github.com/ShawnO86/paint-calc-pages.git main:gh-pages
 
 cd -
